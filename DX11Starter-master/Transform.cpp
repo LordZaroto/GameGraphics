b6c295a45c6248e0a660df31010b832948f0d445
LordZaroto <prestogilmore@gmail.com>
2023-09-27 20:47:21 -0400

    Asgnment 5 Task 1 complete

--- DX11Starter-master/Transform.cpp
+++ DX11Starter-master/Transform.cpp
@@ -8,9 +8,13 @@ Transform::Transform()
 	position = XMFLOAT3(0.0f, 0.0f, 0.0f);
 	pitchYawRoll = XMFLOAT3(0.0f, 0.0f, 0.0f);
 	scale = XMFLOAT3(1.0f, 1.0f, 1.0f);
+	right = XMFLOAT3(1.0f, 0.0f, 0.0f);
+	up = XMFLOAT3(0.0f, 1.0f, 0.0f);
+	forward = XMFLOAT3(0.0f, 0.0f, 1.0f);
 	XMStoreFloat4x4(&world, XMMatrixIdentity());
 	XMStoreFloat4x4(&worldInverseTranspose, XMMatrixIdentity());
 	isMatrixChanged = false;
+	isRotationChanged = false;
 }
 
 Transform::~Transform()
@@ -33,12 +37,14 @@ void Transform::SetRotation(float pitch, float yaw, float roll)
 {
 	pitchYawRoll = XMFLOAT3(pitch, yaw, roll);
 	isMatrixChanged = true;
+	isRotationChanged = true;
 }
 
 void Transform::SetRotation(DirectX::XMFLOAT3 rotation)
 {
 	pitchYawRoll = XMFLOAT3(rotation.x, rotation.y, rotation.z);
 	isMatrixChanged = true;
+	isRotationChanged = true;
 }
 
 void Transform::SetScale(float x, float y, float z)
@@ -93,6 +99,48 @@ DirectX::XMFLOAT4X4 Transform::GetWorldInverseTransposeMatrix()
 	return worldInverseTranspose;
 }
 
+DirectX::XMFLOAT3 Transform::GetRight()
+{
+	if (isRotationChanged)
+	{
+		XMVECTOR workspace = XMQuaternionRotationRollPitchYaw(this->pitchYawRoll.x, this->pitchYawRoll.y, this->pitchYawRoll.z);
+		workspace = XMVector3Rotate(DirectX::XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f), workspace);
+		XMStoreFloat3(&right, workspace);
+
+		isRotationChanged = false;
+	}
+	
+	return right;
+}
+
+DirectX::XMFLOAT3 Transform::GetUp()
+{
+	if (isRotationChanged)
+	{
+		XMVECTOR workspace = XMQuaternionRotationRollPitchYaw(this->pitchYawRoll.x, this->pitchYawRoll.y, this->pitchYawRoll.z);
+		workspace = XMVector3Rotate(DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f), workspace);
+		XMStoreFloat3(&up, workspace);
+
+		isRotationChanged = false;
+	}
+
+	return up;
+}
+
+DirectX::XMFLOAT3 Transform::GetForward()
+{
+	if (isRotationChanged)
+	{
+		XMVECTOR workspace = XMQuaternionRotationRollPitchYaw(this->pitchYawRoll.x, this->pitchYawRoll.y, this->pitchYawRoll.z);
+		workspace = XMVector3Rotate(DirectX::XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f), workspace);
+		XMStoreFloat3(&forward, workspace);
+
+		isRotationChanged = false;
+	}
+
+	return forward;
+}
+
 void Transform::MoveAbsolute(float x, float y, float z)
 {
 	position = XMFLOAT3(position.x + x, position.y + y, position.z + z);
@@ -105,16 +153,34 @@ void Transform::MoveAbsolute(DirectX::XMFLOAT3 offset)
 	isMatrixChanged = true;
 }
 
+void Transform::MoveRelative(float x, float y, float z)
+{
+	XMVECTOR workspace = XMQuaternionRotationRollPitchYaw(this->pitchYawRoll.x, this->pitchYawRoll.y, this->pitchYawRoll.z);
+	workspace = XMVector3Rotate(DirectX::XMVectorSet(x, y, z, 0.0f), workspace);
+	XMStoreFloat3(&position, XMVectorAdd(XMLoadFloat3(&position), workspace));
+	isMatrixChanged = true;
+}
+
+void Transform::MoveRelative(DirectX::XMFLOAT3 offset)
+{
+	XMVECTOR workspace = XMQuaternionRotationRollPitchYaw(this->pitchYawRoll.x, this->pitchYawRoll.y, this->pitchYawRoll.z);
+	workspace = XMVector3Rotate(DirectX::XMVectorSet(offset.x, offset.y, offset.z, 0.0f), workspace);
+	XMStoreFloat3(&position, XMVectorAdd(XMLoadFloat3(&position), workspace));
+	isMatrixChanged = true;
+}
+
 void Transform::Rotate(float pitch, float yaw, float roll)
 {
 	pitchYawRoll = XMFLOAT3(pitchYawRoll.x + pitch, pitchYawRoll.y + yaw, pitchYawRoll.z + roll);
 	isMatrixChanged = true;
+	isRotationChanged = true;
 }
 
 void Transform::Rotate(DirectX::XMFLOAT3 rotation)
 {
 	pitchYawRoll = XMFLOAT3(pitchYawRoll.x + rotation.x, pitchYawRoll.y + rotation.y, pitchYawRoll.z + rotation.z);
 	isMatrixChanged = true;
+	isRotationChanged = true;
 }
 
 void Transform::Scale(float x, float y, float z)
--- DX11Starter-master/Transform.h
+++ DX11Starter-master/Transform.h
@@ -22,10 +22,16 @@ public:
 	DirectX::XMFLOAT3 GetScale();
 	DirectX::XMFLOAT4X4 GetWorldMatrix();
 	DirectX::XMFLOAT4X4 GetWorldInverseTransposeMatrix();
+	DirectX::XMFLOAT3 GetRight();
+	DirectX::XMFLOAT3 GetUp();
+	DirectX::XMFLOAT3 GetForward();
+	
 
 	//Mutators
 	void MoveAbsolute(float x, float y, float z);
 	void MoveAbsolute(DirectX::XMFLOAT3 offset);
+	void MoveRelative(float x, float y, float z);
+	void MoveRelative(DirectX::XMFLOAT3 offset);
 	void Rotate(float pitch, float yaw, float roll);
 	void Rotate(DirectX::XMFLOAT3 rotation);
 	void Scale(float x, float y, float z);
@@ -35,8 +41,12 @@ private:
 	DirectX::XMFLOAT3 position;
 	DirectX::XMFLOAT3 pitchYawRoll;
 	DirectX::XMFLOAT3 scale;
+	DirectX::XMFLOAT3 right;
+	DirectX::XMFLOAT3 up;
+	DirectX::XMFLOAT3 forward;
 	DirectX::XMFLOAT4X4 world;
 	DirectX::XMFLOAT4X4 worldInverseTranspose;
 	bool isMatrixChanged;
+	bool isRotationChanged;
 };
 
